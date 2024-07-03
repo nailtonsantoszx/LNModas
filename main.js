da72240fa983b4ceae5a7d348a53329cdfbc4fc2
LN-Modas <nailtonramos23@gmail.com>
2024-07-02 22:34:48 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', (event) => {
     const filterButtons = document.querySelectorAll('.filter-btn');
     const productCategories = document.querySelectorAll('.product-category');
 
     filterButtons.forEach(button => {
         button.addEventListener('click', () => {
             const filter = button.getAttribute('data-filter');
+
             productCategories.forEach(category => {
                 if (filter === 'all' || category.id === `category-${filter}`) {
                     category.style.display = 'block';
