11d932164c319beec1d2a4ab115b5f126e983bdf
LN-Modas <nailtonramos23@gmail.com>
2024-07-02 21:36:17 -0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,8 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Adiciona a classe 'admin' ao body se for um administrador
-    if (document.body.classList.contains('admin')) {
-        document.querySelectorAll('.admin-only').forEach(el => {
-            el.style.display = 'block';
-        });
-    }
+// admin.js
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.body.classList.add('admin');
 });
