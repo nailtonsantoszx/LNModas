9da415d024b9fee0162c9c189acc8234a362d638
LN-Modas <nailtonramos23@gmail.com>
2024-07-02 22:33:29 -0300

    Update login.php

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    if ($email === 'admin@example.com' && $password === 'senhaadmin') {
+    if ($email === 'nailtonramos23@gmail.com' && $password === 'kenaizin777') {
         $_SESSION['admin'] = true;
         header('Location: index.php');
         exit;
