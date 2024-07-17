48a11abbc222a84fe5353145d6d74e119e1e41d0
LN-Modas <nailtonramos23@gmail.com>
2024-07-17 00:18:08 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = ""; // Atualize com a senha do seu banco de dados
+$dbname = "ln_modas";
 
+// Criar conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexão
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+?>
