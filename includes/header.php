d465db3191bb7e85394c1e82749e16d31539f524
LN-Modas <nailtonramos23@gmail.com>
2024-07-17 00:16:33 -0300

    Update header.php

---
+++ includes/header.php
@@ -0,0 +1,7 @@
+<header>
+    <h1>LN Modas</h1>
+    <div class="auth-links">
+        <a href="login.php" class="auth-button">Entrar</a>
+        <a href="register.php" class="auth-button">Cadastre-se</a>
+    </div>
+</header>
